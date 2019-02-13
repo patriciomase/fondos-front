cf66b98d69a04e80fdef170ce98df755c36605ca
Patricio G Maseda <pmaseda@sparkdigital.com>
2019-02-13 00:39:59 -0300

    Add first ajax call. Remove react boilerplate app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('//localhost:8998/?fund=FBARFPB')
+      .then(response => console.log(response));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
