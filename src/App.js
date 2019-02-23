26fce0ae9a64e340b1df6c99b4126a1ef69a1585
Patricio G Maseda <pmaseda@sparkdigital.com>
2019-02-22 21:49:46 -0300

    Add multiple fund display

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend } from 'recharts';
 import './App.css';
 
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+// const absoluteValue = (p, name) => ({ ...p, [name]: p.value, name: p.dateTime });
+const percentageValue = (name) => (values, elem) => {
+  const lastElem = values.length ? values[values.length - 1] : elem;
+  const last = lastElem[name] || 0;
+  return values.concat({
+    ...elem, [name]: (elem.value * 100 / lastElem.value) - 100 + last, name: elem.dateTime 
+  });
+}
+
+const funds = [
+  { name: 'FACCARB', color: '#0A0' },
+  { name: 'FBARFPB', color: '#F00' },
+  { name: 'FBAHORA', color: '#F0F' },
+  { name: 'BFRENTP', color: '#00F' },
 ];
+
+const merge = (oldValues, newValues) => {
+  const mergedArray = [];
+  newValues.forEach(newElement => {
+    const index = oldValues.findIndex(old => old.dateTime === newElement.dateTime);
+    if (index > -1) {
+      mergedArray.push({ ...oldValues[index], ...newElement});
+    }
+    else {
+      mergedArray.push(newElement);
+    }
+  });
+  // debugger;
+  return mergedArray;
+};
 
 const SimpleLineChart = ({ data }) => {
   return (
-    <LineChart width={1000} height={300} data={data}
-          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+    <LineChart
+      width={1000}
+      height={500}
+      data={data}
+      margin={{top: 5, right: 30, left: 20, bottom: 5}}
+    >
       <XAxis dataKey="name"/>
       <YAxis/>
       <CartesianGrid strokeDasharray="3 3"/>
-      <Tooltip/>
       <Legend />
-      <Line dataKey="FBARFPB:AR" stroke="#8884d8" activeDot={{r: 8}}/>
-      <Line dataKey="uv" stroke="#82ca9d" />
+      {funds.map(f => <Line key={f.name} dataKey={`${f.name}:AR`} stroke={f.color} connectNulls={true} />)}
     </LineChart>
   );
 };
@@ -38,12 +61,16 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('//localhost:8998/?fund=FBARFPB')
-      .then(response => {
-        this.setState({
-          prices: response.data[0].price.map(p => ({ ...p, [response.data[0].ticker]: p.value, name: p.dateTime }))
-        })
-      });
+    funds.map(f => axios.get(`//localhost:8998/?fund=${f.name}`)
+    .then(response => {
+      this.setState(oldState => { return ({
+        // prices: response.data[0].price.map()
+        prices: merge(
+          oldState.prices,
+          response.data[0].price.reduce(percentageValue(response.data[0].ticker), [])
+          )
+      })})
+    }));
   }
 
   render() {
